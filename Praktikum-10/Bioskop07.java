2868a1d693c5ee940ab9b1ab9557f74df5ca246a
Chikoabillabasya <galangrizky767@gmail.com>
2023-10-30 13:53:36 +0700

    Tugas Praktikum Semester 1

--- Praktikum-10/Bioskop07.java
+++ Praktikum-10/Bioskop07.java
@@ -27,6 +27,12 @@ public class Bioskop07 {
 
     for (int i = 0; i < penonton.length; i++) {
         System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+
+    System.out.println(penonton.length);
+
+    for (String[] barisPenonton : penonton) {
+        System.out.println("Panjang baris: " + barisPenonton.length);
+    }
     }
     }
 }
