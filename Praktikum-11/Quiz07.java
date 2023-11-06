ba26faa8f92f6fe1d41031691f7016a160cda1a1
Chikoabillabasya <galangrizky767@gmail.com>
2023-11-06 14:49:57 +0700

    Tugas Praktikum Semester 1

--- Praktikum-11/Quiz07.java
+++ Praktikum-11/Quiz07.java
@@ -1,22 +1,29 @@
 import java.util.Scanner;
 import java.util.Random;
+
 public class Quiz07 {
     public static void main(String[] args) {
-        Random random=new Random();
-        Scanner input=new Scanner(System.in);
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
 
         char menu='y';
-        do{
-            int number = random.nextInt(10)+1;
+        do {
+            int number = random.nextInt(10) + 1;
             boolean success = false;
-            do{
+            do {
                 System.out.print("Tebak angka (1-10): ");
                 int answer = input.nextInt();
                 input.nextLine();
-                success = (answer == number);
-            }while(!success);
+                if (answer < number) {
+                    System.out.println("Tebakan terlalu kecil!");
+                } else if (answer > number) {
+                    System.out.println("Tebakan terlalu besar!");
+                } else {
+                    success = true;
+                }
+            } while (!success);
             System.out.print("Apakah Anda ingin mengulang permainan (Y/y)?");
             menu = input.nextLine().charAt(0);
-        }while(menu=='y' || menu=='Y');
+        } while (menu == 'y' || menu == 'Y');
     }
 }
