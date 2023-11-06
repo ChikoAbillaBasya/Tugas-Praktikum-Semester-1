ccf3941ca3d76200606a7778009c03b29d1f0439
Chikoabillabasya <galangrizky767@gmail.com>
2023-11-06 16:19:42 +0700

    Tugas Praktikum Semester 1

---
+++ Praktikum-11/Tugas307.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Tugas307 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N : ");
+        int n = sc.nextInt();
+
+        if (n >= 3) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (i == 1 || i == n || j == 1 || j == n) {
+                        System.out.print(n + " ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("Nilai N tidak mencapai minimal!");
+        }
+
+        sc.close();
+    }
+}
