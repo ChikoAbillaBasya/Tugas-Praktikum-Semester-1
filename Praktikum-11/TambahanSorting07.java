47f2c64f0ba92ff7a337058aa64481215e26e2d1
Chikoabillabasya <galangrizky767@gmail.com>
2023-11-13 12:46:48 +0700

    Tugas Praktikum Semester 1

---
+++ Praktikum-11/TambahanSorting07.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class TambahanSorting07 {
+    public static void main(String[] args) {
+        String[][] PemainAtlit = {
+                { "Agos", "Daffa", "Sukiman", "Jaki", "Udin" }, // badminton
+                { "Hafiz", "Aida", "Devan", "Marta", "Moheng" }, // tenisMeja
+                { "Ahmad", "Amel", "Dayat", "Rafli", "Adit" }, // basket
+                { "Vika", "Andre", "Aro", "Erent", "Rara" } // voli
+        };
+
+        String[] Cabangolahraga = { "Badminton", "Tenis Meja", "Basket", "Bola Voly" };
+
+        for (int i = 0; i < PemainAtlit.length; i++) {
+            Arrays.sort(PemainAtlit[i]); // mengurutkan arraynya
+        }
+
+        for (int i = 0; i < PemainAtlit.length; i++) { // perulangan untuk menampilkan cabornya
+            System.out.println(Cabangolahraga[i] + ":");
+            for (int j = 0; j < PemainAtlit[i].length; j++) { // perulangan untuk menampilkan nama setiap PemainAtlit yang ikut
+                                                        // porseni
+                System.out.println("Atlit " + Cabangolahraga[i] + " ke-" + (j + 1) + " adalah " + PemainAtlit[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
