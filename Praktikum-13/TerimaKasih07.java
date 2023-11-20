53de355358b42b97182346ffe8a897bbe1a68d3e
Chikoabillabasya <galangrizky767@gmail.com>
2023-11-20 13:05:11 +0700

    Tugas Praktikum Semester 1

--- Praktikum-13/TerimaKasih07.java
+++ Praktikum-13/TerimaKasih07.java
@@ -5,7 +5,7 @@ public class TerimaKasih07 {
     }
     //Pemanggilan dengan fungsi
     public static void main(String[] args) {
-        ucapanTerimaKasih();
+        UcapanTerimaKasih();
     //Tanpa fungsi
         System.out.println("Thank you for being the best teacher in the world.\n"+
         "You inspire in me a love for learning and made me feel like i could ask you anything.");
